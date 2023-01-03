71cc614d55e8b67cc409a7f49fae1d586670a904
arpavlova <115587580+arpavlova@users.noreply.github.com>
2023-01-03 13:47:41 +0200

    Update solution.cpp

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,12 +1,57 @@
 #include "solution.h"
 
-void Trie::insert(const char* word) {
-    return;
+void Trie::insert(const char* word) 
+{
+    NodeTrie* node = root;
+    while (*word)
+    {
+        if (!node->contains(*word))
+        {
+            node->addChar(*word);
+        }
+        node = node->children[*word - 'a'];
+        ++word;
+    }
+    node->isFinal = true;
 };
 
-bool Trie::search(const char* word) {
-    return false;
+bool Trie::search(const char* word) 
+{
+    NodeTrie* node = root;
+    while (*word)
+    {
+        if (!node->contains(*word))
+        {
+            return false;
+        }
+        node = node->children[*word - 'a'];
+        ++word;
+    }
+    return node->isEnd();
 };
+void Trie::cleanNodeTrie(NodeTrie* node)
+{
+    if (node != nullptr)
+    {
+        if (node->isEmpty)
+        {
+            // delete the leaf node
+            delete(node);
+            return;
+        }
 
-Trie::~Trie() {
+        for (NodeTrie* child : node->children)
+        {
+            cleanNodeTrie(child);
+        }
+    }
+}
+void Trie::clean()
+{
+    cleanNodeTrie(root);
+}
+
+Trie::~Trie() 
+{
+    clean();
 }
