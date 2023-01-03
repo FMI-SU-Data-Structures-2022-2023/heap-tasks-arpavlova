3c0216ae0adec596dc9bd7f01e844e487e9d1aa2
arpavlova <115587580+arpavlova@users.noreply.github.com>
2023-01-03 13:47:10 +0200

    Update solution.h

--- src/solution.h
+++ src/solution.h
@@ -1,9 +1,40 @@
 #ifndef WEEK7_TREE_H
 
+
 class Trie {
+private:
+    class NodeTrie
+    {
+    public:
+        NodeTrie* children[26];
+        bool isFinal = false;
+        bool isEmpty = true;
+        bool contains(const char ch) const
+        {
+            return children[ch - 'a'] != nullptr;
+        }
+        void addChar(const char ch)
+        {
+            children[ch - 'a'] = new NodeTrie();
+            isEmpty = false;
+        }
+        bool isEnd() const { return isFinal; }
+        NodeTrie() 
+        {  
+            for (size_t i = 0; i < 26; ++i) { children[i] = nullptr; }
+        }
+    };
+private:
+    NodeTrie* root = new NodeTrie;
+private:
+    void clean();
+    void cleanNodeTrie(NodeTrie* node);
 public:
-    void insert(const char*);
-    bool search(const char*);
+    Trie() = default;
+
+
+    void insert(const char* ch);
+    bool search(const char* ch);
     Trie& operator=(const Trie& other) = delete;
     ~Trie();
 };
